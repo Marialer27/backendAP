fdc4f9a7a8119a3309397ac2677c9890006b67ac
Ale <maria.alerodriguez@gmail.com>
2022-07-31 21:23:19 -0300

    make it better

---
+++ src/main/java/com/ejemplo/proyecto/Repository/IPersonaRepository.java
@@ -0,0 +1,10 @@
+package com.ejemplo.proyecto.Repository;
+
+import com.ejemplo.proyecto.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona,Long> {
+    
+}
---
+++ src/main/java/com/ejemplo/proyecto/Security/Service/UserDetailsImpl.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ejemplo.proyecto.Security.Service;
+
+import com.ejemplo.proyecto.Security.Entity.Usuario;
+import com.ejemplo.proyecto.Security.Entity.UsuarioPrincipal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@ComponentScan({"com.delivery.request"})
+@Service
+public class UserDetailsImpl implements UserDetailsService{
+    @Autowired
+    UsuarioService usuarioService;
+
+    @Override
+    public UserDetails loadUserByUsername(String nombreUsuario) throws UsernameNotFoundException {
+        Usuario usuario = usuarioService.getByNombreUsuario(nombreUsuario).get();
+        return UsuarioPrincipal.build(usuario);
+    }
+    
+    
+}
