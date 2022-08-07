1f4c46ea2aa218bc8f0b781f0b5eb8958163bff5
Ale <maria.alerodriguez@gmail.com>
2022-08-07 19:42:49 -0300

    direcciones

--- src/main/java/com/ejemplo/proyecto/Security/Controller/AuthController.java
+++ src/main/java/com/ejemplo/proyecto/Security/Controller/AuthController.java
@@ -35,7 +35,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins="https://appmarback.herokuapp.com/")
+@CrossOrigin
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
